2d2ac292a7f41a654054a1d43754780518acb1c0
Hugo Elhaj-Lahsen <helhaj-lahsen@mail.ks.int>
2023-06-05 13:59:14 +0200

    a

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,6 +54,25 @@ require'lazy'.setup{
     'folke/which-key.nvim', -- Useful for learning keybinds
     opts = {}
   },
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      end,
+    },
+  },
 }
 
 -- General vim options
@@ -140,6 +159,11 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
+  mapping = {
+    ["<cr>"] = cmp.mapping.confirm{ select = true };
+    ["<s-tab>"] = cmp.mapping.select_prev_item();
+    ["<tab>"] = cmp.mapping.select_next_item();
+  }
 }
 
 -- vim: ts=2 sts=2 sw=2 et
